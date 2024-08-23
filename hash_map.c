00cce73c3a869dfcee6e518a034ddc489322cc65
Sodastream <E.Krollmann@protonmail.com>
2024-08-23 23:13:08 +0200

    added resizing + refactoring

--- hash_map.c
+++ hash_map.c
@@ -6,12 +6,12 @@
 #include <sys/random.h>
 
 
-
-hash_map* hm_alloc(const size_t n) {
+hash_map* hm_alloc(const size_t size) {
     hash_map* map = calloc(1, sizeof(hash_map));
-    map->size = n;
+    map->size = size;
+    map->n = 0;
 
-    map->ptr = calloc(map->size, sizeof(map_entry*));
+    map->chain_arr = calloc(map->size, sizeof(map_entry*));
 
     uint32_t seed;
     getrandom(&seed, sizeof(uint32_t), 0);
@@ -20,47 +20,87 @@ hash_map* hm_alloc(const size_t n) {
     return map;
 }
 
+void hm_resize(hash_map *map, const size_t size) {
+
+    map_entry** new_chain_arr = calloc(size, sizeof(map_entry*));
+
+    for (int i = 0; i < map->size; i++) {
+        chain_move_to_new_arr(map->chain_arr[i], new_chain_arr, size);
+        map->chain_arr[i] = NULL;
+    }
+
+    free(map->chain_arr);
+    map->size = size;
+    map->chain_arr = new_chain_arr;
+}
+
+void chain_move_to_new_arr(map_entry* chain, map_entry** new_chain_arr, const size_t new_size) {
+    if (chain->next != NULL) {
+        chain_move_to_new_arr(chain->next, new_chain_arr, new_size);
+    }
+
+    map_entry* to_copy = chain;
+    to_copy->next = NULL;
+
+    uint32_t new_idx = to_copy->hash % new_size;
+
+    map_entry *existing;
+    if(existing = new_chain_arr[new_idx], existing != NULL) {
+        to_copy->next = existing;
+    } 
+
+    new_chain_arr[new_idx] = to_copy;
+}
+
 
 void hm_set(hash_map* map, const void* key, const float val) {
-    map_entry new_entry = { .key = key, .val = val, .next = NULL };
-    uint32_t idx = hash(key, strlen(key), map->seed) % map->size;
+    map_entry new_entry = { .key = key, .val = val, .next = NULL , .hash = hash(key, strlen(key), map->seed)};
+    uint32_t idx =  new_entry.hash % map->size;
 
     map_entry *entry;
-    if(entry = map->ptr[idx], entry != NULL) {
+    if(entry = map->chain_arr[idx], entry != NULL) {
         new_entry.next = entry;
     } 
 
-    map->ptr[idx] = &new_entry;
+    map->chain_arr[idx] = &new_entry;
+    
+    if ((float)++map->n/(float)map->size >= LOAD_FACTOR_THRESHOLD) {
+        hm_resize(map, (float)map->n/LOAD_FACTOR_RESET);
+    }
 }
 
 float hm_get(const hash_map* map, const void* key) {
-    uint32_t idx = hash(key, strlen(key), map->seed) % map->size;
+    uint32_t idx =  hash(key, strlen(key), map->seed) % map->size;
 
-    map_entry* entry = map->ptr[idx];
+    map_entry* entry = map->chain_arr[idx];
     while (entry = entry->next, entry != NULL) {
         if (!strcmp(entry->key, key)) {
             break;
         }
     }
-
     return entry->val;
 }
 
-void hm_free(const hash_map* map) {
+void hm_free(hash_map* map) {
+
     for (int i = 0; i < map->size; i++) {
-        free_chain(*(map->ptr + i));
-        map->ptr[i] = NULL;
+        chain_free(*(map->chain_arr + i));
+        map->chain_arr[i] = NULL;
     }
+    free(map->chain_arr);
+    map->chain_arr = NULL;
+
     free(map);
 }
 
-void free_chain(map_entry* entry) {
-    if (entry->next != NULL) {
-        free_chain(entry->next);
-        entry->next = NULL;
+void chain_free(map_entry* chain) {
+    if (chain->next != NULL) {
+        chain_free(chain->next);
+        chain->next = NULL;
     }
-    free(entry);
+    free(chain);
 }
+
 
 // Murmur hash 2:
 // https://github.com/abrandoned/murmur2/blob/master/MurmurHash2.c
--- hash_map.h
+++ hash_map.h
@@ -6,27 +6,33 @@ typedef struct map_entry {
     char * key;
     uint32_t val;
     map_entry * next; // for collisions
+    uint32_t hash; // un-modded hash value, used for resizing
 } map_entry;
 
 typedef struct {
-    map_entry ** ptr;
+    map_entry ** chain_arr;
+    uint32_t n;
     size_t size;
     uint32_t seed;
 } hash_map;
 
 
-hash_map* hm_alloc(const size_t n);
+#define LOAD_FACTOR_THRESHOLD 0.7
+#define LOAD_FACTOR_RESET 0.5
+
+uint32_t hash(const void* key, int n, uint32_t seed);
+
+hash_map* hm_alloc(const size_t size);
 
 void hm_set(hash_map* map, const void* key, const float val);
 
 float hm_get(const hash_map* map, const void* key);
-
 //TODO
 map_entry* hm_collect_entries(const hash_map* map);
 
 void hm_resize(hash_map* map, const size_t s);
+void chain_move_to_new_arr(map_entry* chain, map_entry** new_chain_arr, const size_t new_size);
 
-uint32_t hash(const void* key, int n, uint32_t seed);
 
-void hm_free(const hash_map* map);
-void free_chain(map_entry* entry);
+void hm_free(hash_map* map);
+void chain_free(map_entry* chain);
